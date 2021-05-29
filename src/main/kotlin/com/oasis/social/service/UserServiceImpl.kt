8a34406aae86e5a9d4b42fb0aa92f7ef72a6ab8a
mayazhou <mayazhou@oasgames.com>
2021-05-29 15:59:50 +0800

    feat(UserServiceImpl): userapi.json to userapi.yaml

--- src/main/kotlin/com/oasis/social/service/UserServiceImpl.kt
+++ src/main/kotlin/com/oasis/social/service/UserServiceImpl.kt
@@ -36,7 +36,7 @@ class UserServiceImpl() : IUserService, CoroutineVerticle() {
       .setAddress(IUserService::class.java.name)
       .register(IUserService::class.java, userService)
 
-    val routerBuilder = RouterBuilder.create(this.vertx, "userapi.json").await()
+    val routerBuilder = RouterBuilder.create(this.vertx, "userapi.yaml").await()
     // 挂载服务到 event bus
     routerBuilder.mountServicesFromExtensions()
     // 生成路由
