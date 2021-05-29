b57a5f607c0c4c45e1faffbb40e395f9ca3b54bc
mayazhou <mayazhou@oasgames.com>
2021-05-29 15:09:54 +0800

    style: 增加注释

--- src/main/kotlin/com/oasis/social/service/ProductServiceImpl.kt
+++ src/main/kotlin/com/oasis/social/service/ProductServiceImpl.kt
@@ -30,9 +30,12 @@ class ProductServiceImpl() : IProductService, CoroutineVerticle() {
     val serviceBinder = ServiceBinder(this.vertx)
     val productPersistence = IProductPersistence.create()
     val productService = IProductService.create(productPersistence)
+    //订阅eventbus某个地址的消息
     consumer = serviceBinder
       .setAddress(IProductService::class.java.name)
       .register(IProductService::class.java, productService)
+    // 解析productapi.json文件，将结果存入一个LinkedHashMap中，key是operationId，value是OperationImpl
+    // OperationImpl主要有operationId,method,path,pathModel,operationModel
     val routerBinder = RouterBuilder.create(this.vertx, "productapi.json").await()
     // 挂载服务到 event bus
     routerBinder.mountServicesFromExtensions()
