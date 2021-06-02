99c5c5301df605aae27d167220e879c7d359918b
mayazhou <mayazhou@oasgames.com>
2021-06-02 14:25:23 +0800

    feat: 增加用户授权

---
+++ src/main/kotlin/com/oasis/social/RoleVerticle.kt
@@ -0,0 +1,19 @@
+package com.oasis.social
+
+import com.oasis.social.util.GlobalRouter
+import io.vertx.ext.auth.authorization.PermissionBasedAuthorization
+import io.vertx.ext.auth.jwt.authorization.JWTAuthorization
+import io.vertx.ext.web.handler.AuthorizationHandler
+import io.vertx.kotlin.coroutines.CoroutineVerticle
+
+class RoleVerticle : CoroutineVerticle() {
+  override suspend fun start() {
+    val router = GlobalRouter.getRouter()
+
+    val requireAdminHandler = AuthorizationHandler.create(PermissionBasedAuthorization.create("admin"))
+      .addAuthorizationProvider(JWTAuthorization.create("permissions"))
+
+    //需要admin权限
+    router.route("/api/users").handler(requireAdminHandler)
+  }
+}
